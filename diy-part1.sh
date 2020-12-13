a559a9fbb16d8296d5c2f51a6897e8f409366f26
KevinMX <aaawqw@live.cn>
2020-12-13 22:54:09 +0800

    diy-part1: fix git clone path

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,5 @@
 git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
